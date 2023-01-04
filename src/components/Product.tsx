49a641a465e67cb91b91d5c85df566887d2b32b6
Texterly <88582978+Texterly@users.noreply.github.com>
2023-01-04 10:53:29 +0300

    Create product types

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { IProduct } from '../models'
 
-function Product(props: any) {
+interface ProductProps {
+  product: IProduct
+}
+
+function Product({ product }: ProductProps) {
   return (
-    <div className='border py2 px-4 rounded flex flex-col items-center mb-2'>Product</div>
+    <div className='border py2 px-4 rounded flex flex-col items-center mb-2'>
+      <img src={product.image} alt={product.title} className='w-1/6' />
+      <p>{ product.title }</p>
+      <span className='font-bold'>{product.price}</span>
+    </div>
   )
 }
 
