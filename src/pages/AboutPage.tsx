95d894e72b550a319e1923e77939cd91b97e291b
Texterly <88582978+Texterly@users.noreply.github.com>
2023-01-06 18:22:49 +0300

    Added Provider

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor et ab ipsam animi cum eius voluptas qui iusto recusandae facilis laborum cumque deleniti veniam sequi molestias harum commodi quaerat culpa, dolores maxime illum fugit debitis voluptates. Nulla, quas, debitis blanditiis ipsa maiores sit reiciendis minima dolorum fuga repudiandae vitae error.</div>
+  )
+}
+
+export default AboutPage
